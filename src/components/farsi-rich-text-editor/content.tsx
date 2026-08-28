4fb53e9f2cdbc565ff2b5e40b70cb2ccbe7a6ea4
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2026-08-28 09:37:26 +0330

    feat: focus editor on content area click

--- src/components/farsi-rich-text-editor/content.tsx
+++ src/components/farsi-rich-text-editor/content.tsx
@@ -3,7 +3,14 @@ import { EditorContent } from '@tiptap/react'
 
 export const FRTE_Content = ({ editor }: { editor: Editor }) => {
   return (
-    <div className="border bg-white p-4">
+    <div
+      className="cursor-text border bg-white p-4"
+      onClick={() => {
+        if (!editor.isFocused) {
+          editor.chain().focus('end').run()
+        }
+      }}
+    >
       <EditorContent
         editor={editor}
         className="prose h-96 max-h-96 scrollbar-thin overflow-auto [&_.ProseMirror]:outline-none"
