6cd5b5ec72af4fd33486d6af2efb62fc3158cec3
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2026-08-28 08:24:39 +0330

    style: increase content padding from p-2 to p-4

--- src/components/farsi-rich-text-editor/content.tsx
+++ src/components/farsi-rich-text-editor/content.tsx
@@ -3,7 +3,7 @@ import { EditorContent } from '@tiptap/react'
 
 export const FRTE_Content = ({ editor }: { editor: Editor }) => {
   return (
-    <div className="border bg-white p-2">
+    <div className="border bg-white p-4">
       <EditorContent
         editor={editor}
         className="prose h-96 max-h-96 scrollbar-thin overflow-auto [&_.ProseMirror]:outline-none"
